8b1d614953a576d814d2de4689fbd9d3bbcf815e
Gu Zhenhao <guzh@comp.nus.edu.sg>
2024-01-22 17:08:58 +0800

    Read dataset downloading script

--- build_tc_db.sh
+++ build_tc_db.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # This script build the database for each tool
-DB_NAME=test
+DB_NAME=pseudomonas
 FASTA_FILES=/home/zhenhao/tc-data/ncbi_dataset/data/*/*.fna
 LOG_DIRECTORY=/home/zhenhao/tc-benchmark/log
-DB_DIRECTORY=/mnt/c/Users/zhenh/db
+DB_DIRECTORY=/mnt/c/Users/guzh/tax_db
 
 mkdir -p $LOG_DIRECTORY
 mkdir -p $DB_DIRECTORY
@@ -12,8 +12,12 @@ cd $DB_DIRECTORY
 
 # Build Kraken2 custom database
 kraken2-build --download-taxonomy --db ${DB_NAME}
-for file in ${FASTA_FILES}; do kraken2-build --add-to-library $file --db test; done
-/usr/bin/time -o "${LOG_DIRECTORY}/kraken2_build.time" -v kraken2-build --build --db test
+for file in ${FASTA_FILES}
+do 
+    echo "Adding file ${file}."
+    kraken2-build --add-to-library $file --db ${DB_NAME}
+done
+/usr/bin/time -o "${LOG_DIRECTORY}/kraken2_build.time" -v kraken2-build --build --db ${DB_NAME}
 
 
 
---
+++ download_reads.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+## Download sra toolkit
+sudo apt-get install sra-toolkit
+
+## Download reads using prefetch
+cd /mnt/c/Users/guzh/ # Download outside WSL as the size of files is large
+
+mkdir -p tax_data
+cd tax_data
+
+# Download the study that contains pseudomonas isolates
+prefetch -v SRP323385
+
+# Convert all .sra files to .fastq files
+for file in ./*/*.sra
+do
+    fasterq-dump ${file}
+    rm ${file}
+done
+
+## Record ground truth and sample reads from the isolates
+NUM_SAMPLES_PER_FILE=1000
+mkdir -p sampled_reads
+
+# Use NCBI esearch to get the tax ID of the read files
+sudo apt install ncbi-entrez-direct
+
+for sra_id in *
+do
+    tax_id=$(esearch -db sra -query $sra_id | efetch -format runinfo | awk -F "," '{print $28}' | sed -n 2p)
+    head "./${sra_id}/${sra_id}.fastq" -n $(( NUM_SAMPLES_PER_FILE * 4 )) >> "./sampled_reads/${tax_id}.fastq"
+    echo "Sampled dataset ${sra_id} with Tax ID ${tax_id}."
+done
