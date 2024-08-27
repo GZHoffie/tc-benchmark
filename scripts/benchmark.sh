6df3f9e63892a7cbe3e4b5977417dabb2c07a1e8
Gu Zhenhao <zhenhao.gu@u.nus.edu>
2024-08-28 00:01:02 +0800

    Benchmarking kraken2 with different values of k

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -1,4 +1,20 @@
-benchmark_dir=isolates_data
+benchmark_dir=/mnt/c/Users/zhenh/kraken2_benchmark
 
+QUERY_FILES=(/home/zhenhao/htc/data/zymo_test_reads/train_reads.fastq
+             /home/zhenhao/htc/data/zymo_test_reads/ood_species_reads.fastq
+             /home/zhenhao/htc/data/zymo_test_reads/negative.fastq
+             /home/zhenhao/htc/data/human_reads.fastq)
+KRAKEN_DB_DIR=/mnt/c/Users/zhenh/tax_db/
+KRAKEN_DB_NAMES=(test_35_db test_31_db test_27_db test_23_db test_19_db test_15_db)
 
-/usr/bin/time -o ${benchmark_dir}/kraken2.time -v kraken2 --db gtdb_kraken2_cpu isolates_data/sampled_reads.fastq --threads 64 >> ${benchmark_dir}/kraken2.output 2> ${benchmark_dir}/kraken2.log
+mkdir -p ${benchmark_dir}
+mkdir -p ${benchmark_dir}/log
+
+for i in {0..5}
+do
+    db_name=${KRAKEN_DB_NAMES[$i]}
+    for j in {0..3}
+    do
+        /usr/bin/time -o ${benchmark_dir}/log/db${i}_query${j}.time -v kraken2 --db ${KRAKEN_DB_DIR}/${db_name} ${QUERY_FILES[$j]} --threads 64 >> ${benchmark_dir}/db${i}_query${j}.output 2> ${benchmark_dir}/log/db${i}_query${j}.time.log
+    done
+done
--- scripts/build_tc_db.sh
+++ scripts/build_tc_db.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 # This script build the database for each tool
-DB_NAME=pseudomonas
-FASTA_FILES=/home/zhenhao/tc-data/ncbi_dataset/data/*/*.fna
+DB_NAME=test_db
+FASTA_FILES=/home/zhenhao/htc/data/zymo_data/*/train/*/*.fna
 LOG_DIRECTORY=/home/zhenhao/tc-benchmark/log
-DB_DIRECTORY=/mnt/c/Users/guzh/tax_db
+DB_DIRECTORY=/mnt/c/Users/zhenh/tax_db
 
 mkdir -p $LOG_DIRECTORY
 mkdir -p $DB_DIRECTORY
 cd $DB_DIRECTORY
 
 # Build Kraken2 custom database
-kraken2-build --download-taxonomy --db ${DB_NAME}
+#kraken2-build --download-taxonomy --db ${DB_NAME}
 for file in ${FASTA_FILES}
 do 
     echo "Adding file ${file}."
-    kraken2-build --add-to-library $file --db ${DB_NAME}
+    kraken2-build --add-to-library "$file" --db ${DB_NAME}
 done
 /usr/bin/time -o "${LOG_DIRECTORY}/kraken2_build.time" -v kraken2-build --build --db ${DB_NAME}
 
